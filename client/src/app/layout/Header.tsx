7c4597847039867f433d5a2609bc1f3628071557
Kramer Jensen <kramerdjensen@gmail.com>
2024-01-17 16:40:00 -0500

    Styled Cards using Material UI

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,7 +2,7 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 
 export default function Header() {
     return (
-        <AppBar position="fixed">
+        <AppBar position="sticky" sx={{mb: 4}}>
             <Toolbar>
                 <Typography variant="h6">
                     Everything Gaming
