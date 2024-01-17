3349919f1ff13d8f2336cc9cf92fb0d432ba8985
Kramer Jensen <kramerdjensen@gmail.com>
2024-01-16 21:36:17 -0500

    Implemented React Client

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import { Product } from "../models/product";
+
+
+function App() {
+  const [products, setProducts] = useState<Product[]>( [] );
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data))
+  }, [])
+
+  function addProduct() {
+    setProducts( prevState => [...prevState, {
+      id: prevState.length + 101,
+      name: 'product3' + (prevState.length + 1), 
+      price: (prevState.length * 100) + 100,
+      brand: 'some brand',
+      description: 'some description',
+      pictureUrl: 'http://picsum.photos/200'
+    }])
+  }
+
+  return (
+    <div>
+      <h1>Gaming Everything</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
+    </div>
+  )
+}
+
+export default App
