0eb7c94c958cc401c8ab438e8b1bc2ce7f08c230
Kramer Jensen <kramerdjensen@gmail.com>
2024-01-18 11:46:38 -0500

    Implemented Dark/Light Mode Switch

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,17 +1,22 @@
-import { Button } from "@mui/material";
 import { Product } from "../../app/models/product"
 import ProductList from "./ProductList";
+import { useState, useEffect } from "react";
 
-interface Props {
-    products: Product[];
-    addProduct: () => void;
-}
 
-export default function Catalog({products, addProduct}: Props) {
+
+export default function Catalog() {
+
+    const [products, setProducts] = useState<Product[]>( [] );
+
+    useEffect(() => {
+      fetch('http://localhost:5000/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+    }, [])
+
     return (
         <>
             <ProductList products={products} />
-            <Button variant="contained" onClick={addProduct}>Add Product</Button>
         </>
         
     )
