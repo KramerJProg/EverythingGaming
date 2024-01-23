ce5df0a4b018871cb3d90f4b0bc8edd70eb3491c
Kramer Jensen <kramerdjensen@gmail.com>
2024-01-23 07:41:03 -0500

    Implemented Remove Item from cart and increment

--- client/src/app/util/util.ts
+++ client/src/app/util/util.ts
@@ -1,3 +1,4 @@
+// The purpose of this function is to get the user's cookie 
 export function getCookie(key: string) {
     const b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
     return b ? b.pop() : "";
