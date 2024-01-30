2b6234a44afefaa65a4c1610c76ab38435b2901a
Kramer Jensen <kramerdjensen@gmail.com>
2024-01-29 22:43:57 -0500

    implemented add/remove item from item detail page

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,23 +1,55 @@
-import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/NotFound";
 import LoadingComponent from "../../app/layout/LoadingComponents";
+import { useStoreContext } from "../../app/context/StoreContext";
+import { LoadingButton } from "@mui/lab";
 
 export default function ProductDetails() {
+    const {cart, setCart, removeItem} = useStoreContext();
     const {id} = useParams<{id: string}>();
     const [product, setProduct] = useState<Product | null>(null);
     const [loading, setLoading] = useState(true);
+    const [quantity, setQuantity] = useState(0);
+    const [submitting, setSubmitting] = useState(false);
+    const item = cart?.items.find(i => i.productId === product?.id);
 
     // Axios is used from agent.
     useEffect(() => {
+        if (item) setQuantity(item.quantity);
+
         id && agent.Catalog.details(parseInt(id))
             .then(response => setProduct(response))
             .catch(error => console.log(error))
             .finally(() => setLoading(false));
-    }, [id])
+    }, [id, item])
+
+    function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+        if (parseInt(event.currentTarget.value) >= 0) {
+            setQuantity(parseInt(event.currentTarget.value));
+        }
+    }
+
+    function handleUpdateCart() {
+        if (!product) return;
+        setSubmitting(true);
+        if (!item || quantity > item.quantity) {
+            const updatedQuantity = item ? quantity - item.quantity : quantity;
+            agent.Cart.addItem(product.id, updatedQuantity)
+                .then(cart => setCart(cart))
+                .catch(error => console.log(error))
+                .finally(() => setSubmitting(false))
+        } else {
+            const updatedQuantity = item.quantity - quantity;
+            agent.Cart.removeItem(product.id, updatedQuantity)
+                .then(() => removeItem(product.id, updatedQuantity))
+                .catch(error => console.log(error))
+                .finally(() => setSubmitting(false));
+        }
+    }
 
     if (loading) return <LoadingComponent message="Loading Game..."/>
 
@@ -59,6 +91,26 @@ export default function ProductDetails() {
                         </TableBody>
                     </Table>
                 </TableContainer>
+                <Grid container spacing={2} sx={{mt: .5}}>
+                    <Grid item xs={2}>
+                        <TextField 
+                            onChange={handleInputChange} variant="outlined" type="number" label="Quantity in Cart" fullWidth value={quantity} 
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <LoadingButton
+                            disabled={item?.quantity === quantity || !item && quantity === 0}
+                            loading={submitting} 
+                            onClick={handleUpdateCart} 
+                            sx={{height: "55px"}} 
+                            color="primary" 
+                            size="large" 
+                            variant="contained" 
+                            fullWidth>
+                            {item ? "Update Quantity" : "Add to Cart"}
+                        </LoadingButton>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     )
