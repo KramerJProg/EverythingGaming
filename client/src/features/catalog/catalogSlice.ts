8901abc3c30286b52451ce84436f84e148afc62d
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2024-02-18 22:46:16 -0500

    Pagination Fully Functional (#86)
    Co-authored-by: Kramer Jensen <kramerdjensen@gmail.com>

--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/too
 import { Product, ProductParams } from "../../app/models/product";
 import agent from "../../app/api/agent";
 import { RootState } from "../../app/store/configureStore";
+import { MetaData } from "../../app/models/pagination";
 
 // Sending these up to the API as a Query String.
 interface CatalogState {
@@ -11,6 +12,7 @@ interface CatalogState {
     brands: string[];
     types: string[];
     productParams: ProductParams;
+    metaData: MetaData | null;
 }
 
 const productsAdapter = createEntityAdapter<Product>();
@@ -21,8 +23,8 @@ function getAxiosParams(productParams: ProductParams) {
     params.append("pageSize", productParams.pageSize.toString());
     params.append("orderBy", productParams.orderBy.toString());
     if (productParams.searchTerm) params.append("searchTerm", productParams.searchTerm);
-    if (productParams.brands) params.append("brands", productParams.brands.toString());
-    if (productParams.types) params.append("types", productParams.types.toString());
+    if (productParams.brands.length > 0) params.append("brands", productParams.brands.toString());
+    if (productParams.types.length > 0) params.append("types", productParams.types.toString());
     return params;
 }
 
@@ -31,7 +33,9 @@ export const fetchProductsAsync = createAsyncThunk<Product[], void, {state: Root
     async (_, thunkAPI) => {
         const params = getAxiosParams(thunkAPI.getState().catalog.productParams);
         try {
-            return await agent.Catalog.list(params);
+            const response = await agent.Catalog.list(params);
+            thunkAPI.dispatch(setMetaData(response.metaData));
+            return response.items;
         } catch (error: any) {
             return thunkAPI.rejectWithValue({error: error.data})
         }
@@ -65,7 +69,9 @@ function initParams() {
     return {
         pageNumber: 1,
         pageSize: 6,
-        orderBy: "name"
+        orderBy: "name",
+        brands: [],
+        types: []
     }
 }
 
@@ -79,12 +85,20 @@ export const catalogSlice = createSlice({
         types: [],
         // sends up the paramters to the API so it can
         // give the correct information back to the client.
-        productParams: initParams()
+        productParams: initParams(),
+        metaData: null
     }),
     reducers: {
         setProductParams: (state, action) => {
             state.productsLoaded = false;
+            state.productParams = {...state.productParams, ...action.payload, pageNumber: 1};
+        },
+        setPageNumber: (state, action) => {
+            state.productsLoaded = false;
             state.productParams = {...state.productParams, ...action.payload};
+        },
+        setMetaData: (state, action) => {
+            state.metaData = action.payload;
         },
         resetProductParams: (state) => {
             state.productParams = initParams();
@@ -135,4 +149,4 @@ export const catalogSlice = createSlice({
 
 export const productSelectors = productsAdapter.getSelectors((state: RootState) => state.catalog);
 
-export const {setProductParams, resetProductParams} = catalogSlice.actions;
+export const {setProductParams, resetProductParams, setMetaData, setPageNumber} = catalogSlice.actions;
