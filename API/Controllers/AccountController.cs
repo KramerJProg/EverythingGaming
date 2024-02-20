7512affebea2fe6b43a780ec663143805d4ac25f
Kramer Jensen <kramerdjensen@gmail.com>
2024-02-20 12:47:05 -0500

    Implemented JWT Token Service

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,22 +14,43 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly UserManager<User> _userManager;
-        public AccountController(UserManager<User> userManager)
+        private readonly TokenService _tokenService;
+        public AccountController(UserManager<User> userManager, TokenService tokenService)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
         }
 
+        /// <summary>
+        /// Logs the user in. Checks to see if the user exists with the correct credentials,
+        /// if it cannot find the credentials or they don't match, it will trigger the 
+        /// Unauthorized from the BaseController (MVC). Otherwise it will authorize the
+        /// User to login along with providing a JWT Token.
+        /// </summary>
+        /// <param name="loginDto"></param>
+        /// <returns>UserDto with Token.</returns>
         [HttpPost("login")]
-        public async Task<ActionResult<User>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _userManager.FindByNameAsync(loginDto.UserName);
 
             if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
                 return Unauthorized();
 
-            return user;
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = await _tokenService.GenerateToken(user)
+            };
         }
 
+        /// <summary>
+        /// Registering a User. Checks to see if the User's username OR email already exists
+        /// via Microsoft.AspNetCore.Identity -> (IdentityError -> IdentityErrorDecriber) and
+        /// will loop over all possible errors for the checks not being valid.
+        /// </summary>
+        /// <param name="registerDto"></param>
+        /// <returns>Fails from Identity : Validation Error |--| Success from Identity : Adds user with role.</returns>
         [HttpPost("register")]
         public async Task<ActionResult> Register(RegisterDto registerDto)
         {
@@ -48,6 +71,29 @@ namespace API.Controllers
             await _userManager.AddToRoleAsync(user, "Member");
 
             return StatusCode(201);
+        }
+
+        // The authorize attribute will protect this end point.
+        // Can't use Authorize for all methods because Register
+        // Login need to be accessed anonymously of course.
+        /// <summary>
+        /// Purpose of this method is to get the currently logged in user. Gets the user
+        /// information from the token that is sent up with the request. It then uses the token
+        /// to get the user from the database. Can only do this when genuinally authenticated.
+        /// </summary>
+        /// <returns>UserDto with the token.</returns>
+        [Authorize]
+        [HttpGet("currentUser")]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            // Gets the Name Claim from the token.
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = await _tokenService.GenerateToken(user)
+            };
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,13 @@
+using System.Text;
 using API.Data;
 using API.Entities;
 using API.Middleware;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +15,35 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Boiler plate code for Swagger to be able to test an authenticated endpoint.
+builder.Services.AddSwaggerGen(c => 
+{
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Put Bearer + your token in the box below",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    c.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                jwtSecurityScheme, Array.Empty<string>()
+            }
+        }
+    );
+});
 
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
@@ -28,8 +61,29 @@ builder.Services.AddIdentityCore<User>(opt =>
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<StoreContext>();
 
-builder.Services.AddAuthentication();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt => 
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            // Doesn't pass the issuer back with the token.
+            ValidateIssuer = false,
+            // Same as issuer
+            ValidateAudience = false,
+            // Passing back and has expiery, API will check lifetime of token and validate against it.
+            ValidateLifetime = true,
+            // Checks validity of the token based on the signature it used when creating the token.
+            ValidateIssuerSigningKey = true,
+            // So the API knows which key was used to sign it. 
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:TokenKey"]))
+        };
+    });
 builder.Services.AddAuthorization();
+// AddScoped means the service will be active while the HTTP request
+// is being processed, once it is finished, the token service will
+// be disposed of.
+builder.Services.AddScoped<TokenService>();
 
 var app = builder.Build();
 
@@ -40,7 +94,12 @@ app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    
+    // perists the token to avoid keep pasting the token over and over again into swagger 
+    app.UseSwaggerUI(c =>
+    {
+        c.ConfigObject.AdditionalItems.Add("persistAuthorization", "true");
+    });
 }
 
 app.UseCors(opt => 
@@ -48,6 +107,8 @@ app.UseCors(opt =>
     opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
 });
 
+app.UseAuthentication();
+// Before authorizing the user, need to know WHO they are ↑↑↑ (Authentication).
 app.UseAuthorization();
 
 app.MapControllers();
