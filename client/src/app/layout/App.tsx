0c1e59ad605c7a6939455cbe8d5453ffb1203586
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2024-02-21 23:15:35 -0500

    Fixed Login persistence bug (#102)
    Co-authored-by: Kramer Jensen <kramerdjensen@gmail.com>

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,14 +1,13 @@
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Header from "./Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { getCookie } from "../util/util";
-import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponents";
 import { useAppDispatch } from "../store/configureStore";
-import { setCart } from "../../features/cart/cartSlice";
+import { fetchCartAsync } from "../../features/cart/cartSlice";
+import { fetchCurrentUser } from "../../features/account/accountSlice";
 
 
 function App() {
@@ -17,17 +16,18 @@ function App() {
   const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const buyerId = getCookie("buyerId");
-    if (buyerId) {
-      agent.Cart.get()
-        .then(cart => dispatch(setCart(cart)))
-        .catch(error => console.log(error))
-        .finally(() => setLoading(false));
-    } else {
-      setLoading(false);
+  const initApp = useCallback(async () => {
+    try {
+      await dispatch(fetchCurrentUser());
+      await dispatch(fetchCartAsync());
+    } catch (error) {
+      console.log(error)
     }
   }, [dispatch])
+
+  useEffect(() => {
+    initApp().then(() => setLoading(false));
+  }, [initApp])
 
   const [darkMode, setDarkMode] = useState(true);
   const paletteType = darkMode ? "dark" : "light";
--- client/src/features/account/accountSlice.ts
+++ client/src/features/account/accountSlice.ts
@@ -3,6 +3,7 @@ import { User } from "../../app/models/user";
 import { FieldValues } from "react-hook-form";
 import agent from "../../app/api/agent";
 import { router } from "../../app/router/Routes";
+import { toast } from "react-toastify";
 
 interface AccountState {
     user: User | null;
@@ -32,14 +33,20 @@ export const signInUser = createAsyncThunk<User, FieldValues>(
  * Fetches the user via the token, but not the data.
  */
 export const fetchCurrentUser = createAsyncThunk<User>(
-    "account/signInUser",
+    "account/fetchCurrentUser",
     async (_, thunkAPI) => {
+        thunkAPI.dispatch(setUser(JSON.parse(localStorage.getItem("user")!)));
         try {
             const user = await agent.Account.currentUser();
             localStorage.setItem("user", JSON.stringify(user));
             return user;
         } catch (error: any) {
             return thunkAPI.rejectWithValue({error: error.data});
+        }
+    },
+    {
+        condition: () => {
+            if (!localStorage.getItem("user")) return false;
         }
     }
 )
@@ -52,16 +59,25 @@ export const accountSlice = createSlice({
             state.user = null;
             localStorage.removeItem("user");
             router.navigate("/");
+        },
+        setUser: (state, action) => {
+            state.user = action.payload;
         }
     },
     extraReducers: (builder => {
+        builder.addCase(fetchCurrentUser.rejected, (state) => {
+            state.user = null;
+            localStorage.removeItem("user");
+            toast.error("Session expired - Please login again");
+            router.navigate("/");
+        })
         builder.addMatcher(isAnyOf(signInUser.fulfilled, fetchCurrentUser.fulfilled), (state, action) => {
             state.user = action.payload;
         });
-        builder.addMatcher(isAnyOf(signInUser.rejected, fetchCurrentUser.rejected), (_state, action) => {
+        builder.addMatcher(isAnyOf(signInUser.rejected), (_state, action) => {
             console.log(action.payload);
         })
     })
 })
 
-export const {signOut} = accountSlice.actions;
+export const {signOut, setUser} = accountSlice.actions;
