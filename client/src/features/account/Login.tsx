d4dc0ac01c28869fba68ef3028fe0bf37e0c5807
Kramer Jensen <kramerdjensen@gmail.com>
2024-02-21 20:48:53 -0500

    Able to store results of login in redux store

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -6,73 +6,73 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import { Paper } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import Link from '@mui/material/Link';
-import agent from '../../app/api/agent';
 import { FieldValues, useForm } from 'react-hook-form';
 import { LoadingButton } from "@mui/lab";
+import { useAppDispatch } from '../../app/store/configureStore';
+import { signInUser } from './accountSlice';
 
 
 export default function Login() {
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+
     const {register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm({
         mode: "onTouched"
     }) 
 
     async function submitForm(data: FieldValues) {
-        try {
-            await agent.Account.login(data);
-        } catch (error) {
-            console.log(error)
-        }
-        
+        await dispatch(signInUser(data));
+        navigate("/catalog");
     }
 
-  return (
-      <Container component={Paper} maxWidth="sm" sx={{display: "flex", flexDirection: "column", alignItems: "center", p: 4}}>
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              fullWidth
-              label="Username"
-              autoComplete="email"
-              autoFocus
-              {...register("username", {required: "Username is required!"})}
-              error={!!errors.username}
-              helperText={errors?.username?.message as string}
-            />
-            <TextField
-              margin="normal"
-              fullWidth
-              label="Password"
-              type="password"
-              {...register("password", {required: "Password is required!"})}
-              error={!!errors.password}
-              helperText={errors?.password?.message as string}
-            />
-            <LoadingButton 
-              loading={isSubmitting}
-              disabled={!isValid}
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </LoadingButton>
-            <Grid container>
-              <Grid item>
-                <Link component={RouterLink} to="/register">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-      </Container>
-  );
+    return (
+        <Container component={Paper} maxWidth="sm" sx={{display: "flex", flexDirection: "column", alignItems: "center", p: 4}}>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Sign in
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
+                <TextField
+                    margin="normal"
+                    fullWidth
+                    label="Username"
+                    autoComplete="email"
+                    autoFocus
+                    {...register("username", {required: "Username is required!"})}
+                    error={!!errors.username}
+                    helperText={errors?.username?.message as string}
+                />
+                <TextField
+                    margin="normal"
+                    fullWidth
+                    label="Password"
+                    type="password"
+                    {...register("password", {required: "Password is required!"})}
+                    error={!!errors.password}
+                    helperText={errors?.password?.message as string}
+                />
+                <LoadingButton 
+                    loading={isSubmitting}
+                    disabled={!isValid}
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                Sign In
+                </LoadingButton>
+                <Grid container>
+                    <Grid item>
+                        <Link component={RouterLink} to="/register">
+                            {"Don't have an account? Sign Up"}
+                        </Link>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
+    );
 }
