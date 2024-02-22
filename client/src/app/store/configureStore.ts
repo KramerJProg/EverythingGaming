328df1d54b88fdadddb929fe3e5f12008aa44929
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2024-02-21 21:00:17 -0500

    Able to store results of login in redux store (#99)
    Co-authored-by: Kramer Jensen <kramerdjensen@gmail.com>

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { cartSlice } from "../../features/cart/cartSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { catalogSlice } from "../../features/catalog/catalogSlice";
+import { accountSlice } from "../../features/account/accountSlice";
 
 export const store = configureStore ({
     reducer: {
         counter: counterSlice.reducer,
         cart: cartSlice.reducer,
-        catalog: catalogSlice.reducer
+        catalog: catalogSlice.reducer,
+        account: accountSlice.reducer
     }
 })
 
