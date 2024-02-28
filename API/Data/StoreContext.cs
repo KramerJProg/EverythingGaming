01255ec995b58e56ebc9b239665df7ba30a33caf
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2024-02-27 20:51:34 -0500

    Started Setting up Orders (#110)
    Co-authored-by: Kramer Jensen <kramerdjensen@gmail.com>

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,5 +1,6 @@
 
 using API.Entities;
+using API.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,7 @@ namespace API.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
