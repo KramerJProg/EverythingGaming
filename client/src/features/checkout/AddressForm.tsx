bd94876126cb21473fa4cce79357d02db0c5d77d
Kramer Jensen <kramerdjensen@gmail.com>
2024-03-11 20:48:43 -0400

    Added a checkout form for the user client side

---
+++ client/src/features/checkout/AddressForm.tsx
@@ -0,0 +1,45 @@
+import { Typography, Grid, FormControlLabel, Checkbox, Button } from "@mui/material";
+import { useFormContext } from "react-hook-form";
+import AppTextInput from "../../app/components/AppTextInput";
+
+export default function AddressForm() {
+    const {control} = useFormContext();
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping address
+      </Typography>
+        <Grid container spacing={3}>
+            <Grid item xs={12} sm={12}>
+            <AppTextInput control={control} name="fullName" label="Full Name" />
+            </Grid>
+            <Grid item xs={12}>
+                <AppTextInput control={control} name="address1" label="Address 1" />
+            </Grid>
+            <Grid item xs={12}>
+                <AppTextInput control={control} name="address2" label="Address 2" />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <AppTextInput control={control} name="city" label="City" />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <AppTextInput control={control} name="state" label="State" />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <AppTextInput control={control} name="zip" label="Zipcode" />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <AppTextInput control={control} name="country" label="Country" />
+            </Grid>
+            <Grid item xs={12}>
+            <FormControlLabel
+                control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
+                label="Use this address for payment details"
+            />
+            </Grid>
+        </Grid>
+        <Button type="submit">Submit Form</Button>
+    </>
+  );
+}
