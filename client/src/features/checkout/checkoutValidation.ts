65e95c99fa3912cfe46c70add6ef80755776287b
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2024-03-16 15:23:55 -0400

    Checkout Page Functional (#126)
    Co-authored-by: Kramer Jensen <kramerdjensen@gmail.com>

--- client/src/features/checkout/checkoutValidation.ts
+++ client/src/features/checkout/checkoutValidation.ts
@@ -1,11 +1,17 @@
 import * as yup from "yup";
 
-export const validationSchema = yup.object({
-    fullName: yup.string().required("Full Name is required!"),
-    address1: yup.string().required("Address is required!"),
-    address2: yup.string(),
-    city: yup.string().required("City is required!"),
-    state: yup.string().required("State is required!"),
-    zip: yup.string().required("Zipcode is required!"),
-    country: yup.string().required("Country is required!"),
-})
+export const validationSchema = [
+    yup.object({
+        fullName: yup.string().required("Full Name is required!"),
+        address1: yup.string().required("Address is required!"),
+        address2: yup.string(),
+        city: yup.string().required("City is required!"),
+        state: yup.string().required("State is required!"),
+        zip: yup.string().required("Zipcode is required!"),
+        country: yup.string().required("Country is required!"),
+    }),
+    yup.object(),
+    yup.object({
+        nameOnCard: yup.string().required()
+    })
+] 
