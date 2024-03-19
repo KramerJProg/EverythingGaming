be9eb3d9311bb249f845fbd62510bf698202c7cd
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2024-03-19 06:02:45 -0400

    Issue#135 stripe payments (#136)
    * Implemented back bone Stripe config
    
    * Implemented bare bones Stripe service
    
    ---------
    
    Co-authored-by: Kramer Jensen <kramerdjensen@gmail.com>

---
+++ API/Data/Migrations/20240319093724_PaymentIntentAdded.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentIntentAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientSecret",
+                table: "Carts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Carts",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ClientSecret",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Carts");
+        }
+    }
+}
---
+++ API/Services/PaymentService.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Stripe;
+
+namespace API.Services
+{
+    public class PaymentService
+    {
+        private readonly IConfiguration _config;
+
+        public PaymentService(IConfiguration config)
+        {
+            _config = config;
+            
+        }
+
+        /// <summary>
+        /// Allows for the use of the 3rd party service (Stripe).
+        /// Payment Intent is coming for the Stripe extension.
+        /// This basically tells Stripe what information will be
+        /// required from the user in order to practice 
+        /// Strong Customer Authentication (SCA).
+        /// </summary>
+        /// <param name="cart"></param>
+        /// <returns>Payment Intent (SCA)</returns>
+        public async Task<PaymentIntent> CreateOrUpdatePaymentIntent(Cart cart)
+        {
+            StripeConfiguration.ApiKey = _config["StripeSettings:SecretKey"];
+
+            var service = new PaymentIntentService();
+
+            var intent = new PaymentIntent();
+            var subtotal = cart.Items.Sum(item => item.Quantity * item.Product.Price);
+            var deliveryFee = subtotal > 10000 ? 0 : 750;
+
+            if (string.IsNullOrEmpty(cart.PaymentIntentId))
+            {
+                var options = new PaymentIntentCreateOptions
+                {
+                    Amount = subtotal + deliveryFee,
+                    Currency = "usd",
+                    PaymentMethodTypes = new List<string> {"card"}
+                };
+
+                intent = await service.CreateAsync(options);
+                cart.PaymentIntentId = intent.Id;
+                cart.ClientSecret = intent.ClientSecret;
+            }
+            else 
+            {
+                var options = new PaymentIntentUpdateOptions
+                {
+                    Amount = subtotal + deliveryFee
+                };
+                await service.UpdateAsync(cart.PaymentIntentId, options);
+            }
+
+            return intent;
+        }
+    }
+}
