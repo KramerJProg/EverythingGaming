d3950a6a63893d5f09c5cf7e8c78a979f4a0e703
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2024-03-22 17:08:28 -0400

    Intent Payment Function Implemented (#144)
    Co-authored-by: Kramer Jensen <kramerdjensen@gmail.com>

--- client/src/features/checkout/CheckoutWrapper.tsx
+++ client/src/features/checkout/CheckoutWrapper.tsx
@@ -1,10 +1,28 @@
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutPage from "./CheckoutPage";
 import { loadStripe } from "@stripe/stripe-js";
+import { useAppDispatch } from "../../app/store/configureStore";
+import { useEffect, useState } from "react";
+import agent from "../../app/api/agent";
+import { setCart } from "../cart/cartSlice";
+import LoadingComponent from "../../app/layout/LoadingComponents";
 
 const stripePromise = loadStripe("pk_test_51OvyyyLfB7ekKJSqQe9hDHDh6t9ohLAhpT9mYK8yoM31FkWbCt4BBrnJ9ohL5E5NJ5kVdOqDN6XbCt6UFrddp1QH00BQfU8Sa2")
 
 export default function CheckoutWrapper() {
+
+    const dispatch = useAppDispatch();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        agent.Payments.createPaymentIntent()
+            .then(cart => dispatch(setCart(cart)))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }, [dispatch]);
+
+    if (loading) return <LoadingComponent message="Loading Checkout..." />
+
     return (
         <Elements stripe={stripePromise}>
             <CheckoutPage />
